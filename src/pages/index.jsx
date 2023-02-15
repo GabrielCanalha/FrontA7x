b9217f84c68c3156ba8fe58e5202df183707ff4a
Breno Alvim <alvimbrenosilva6@gmail.com>
2023-02-14 22:58:40 -0300

    Framework add

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Meta from "../components/Head/Meta";
+import Link from "../components/Head/Link";
+import Title from "../components/Head/Title";
+
+export default function Home() {
+  return (
+    <>
+      <Meta />
+      <Link />
+      <Title name="Home" />
+      <h1></h1>
+    </>
+  );
+}
